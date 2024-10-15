7f7d72321889a83dcdb65d86f9d79d799c8a4bb8
mahbubulalways <mahbubulhasan604@gmail.com>
2024-10-15 23:21:13 +0600

    add static restrictions

--- src/main/java/object_oriented_programming/static_variable/StaticMethod.java
+++ src/main/java/object_oriented_programming/static_variable/StaticMethod.java
@@ -5,11 +5,14 @@ package object_oriented_programming.static_variable;
  */
 public class StaticMethod {
     
-    public  void displayOne (){
+    public void displayOne (){
         System.out.println("Hello, I am not a static method.");
     }
     
-    static  void displayTwo (){
+    // * Restriction of static method
+    // * 1. Can not access non-static member  in static method
+    // * 2. Not use this and super 
+    static void displayTwo (){
         System.out.println("Hello, I am a static method.");
     }
     
